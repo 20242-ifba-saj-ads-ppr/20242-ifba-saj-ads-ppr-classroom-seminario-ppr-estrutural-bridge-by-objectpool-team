c3ad6d04310d02253d88cede2fc6e5d025a05fa4
isaquelis <isaquelis.g@gmail.com>
2025-03-12 06:13:06 -0300

    mod: correções

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,32 @@
-import bridge.abst.AtaqueMagico;
-import bridge.abst.RpgAction;
-import bridge.impl.RpgCharacter;
-import bridge.impl.Clerigo;
-import bridge.impl.Guerreiro;
+
+import bridge.abst.Clerigo;
+import bridge.abst.Guerreiro;
+import bridge.abst.RpgCharacter;
+import bridge.impl.EnglishRender;
+import bridge.impl.PortugueseRender;
+import bridge.impl.RpgRender;
 
 public class Main{
    
     public static void main(String args[]){
 
-
         RpgCharacter clerigo;
         RpgCharacter guerreiro;
 
-        RpgAction magia;
+        RpgRender render;
 
-        clerigo = new Clerigo();
-        guerreiro = new Guerreiro();
+        clerigo = new Clerigo(100, 40, 40, 30, 60, 80);
+        guerreiro = new Guerreiro(100, 90, 50, 70, 60, 10);
 
-        magia = new AtaqueMagico(clerigo, 0.3);
+        System.out.println("Clerigo ataca guerreiro (RENDERIZADO EM PORTUGUÊS):");
+        render = new PortugueseRender();
+        clerigo.setRender(render);
+        clerigo.renderizarAtaque(guerreiro);
 
-        magia.atacar(guerreiro);
+        System.out.println("Guerreiro ataca clerigo (RENDERIZADO EM INGLÊS):");
+        render = new EnglishRender();
+        guerreiro.setRender(render);
+        guerreiro.renderizarAtaque(clerigo);
 
     }
     
